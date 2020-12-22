f825ca7c73d366d0d41ccf8822fc4f6f256b02ef
ruyu0 <257703172@users.github.com>
2020-12-22 12:06:39 +0800

    add eclipse project

---
+++ Project1/src/character/GiantDragonLazySingleton.java
@@ -0,0 +1,8 @@
+package character;
+
+//懒汉式单例模式
+public class GiantDragonLazySingleton {
+	private GiantDragonLazySingleton() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ Project1/src/character/Hero.java
@@ -0,0 +1,59 @@
+package character;
+
+public class Hero {
+	String name; //姓名
+    
+    float hp; //血量
+       
+    float armor; //护甲
+       
+    int moveSpeed; //移动速度
+    
+    //带一个参数的构造方法
+    public Hero(String heroname){ 
+        name = heroname;
+    }
+     
+    //带两个参数的构造方法
+    public Hero(String heroname,float herohp){ 
+        name = heroname;
+        hp = herohp;
+    }
+    
+ 
+    public static void main(String[] args) {
+           Hero h1 =  new Hero("大盖伦");
+           Hero h2 =  new Hero("提莫");
+           Hero h3;
+           Hero h4;
+           h3 = h1;
+           h4 = h2;
+           System.out.println(h3.hashCode());
+           System.out.println(h4.hashCode());
+           
+           ADHero bh = new ADHero("赏金猎人");
+           
+           bh.attack(h1, h2);
+    }
+}
+
+class ADHero extends Hero
+{
+	public ADHero(String heroname) {
+		super(heroname);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void attack() {
+    	System.out.println(name + "进行了一次攻击");
+    }
+    
+    public void attack(Hero h1) {
+    	System.out.println(name + "攻击了" + h1.name);
+    }
+    
+    public void attack(Hero h1, Hero h2) {
+        System.out.println(name + "同时对" + h1.name + "和" + h2.name + "进行了攻击 ");
+    }
+}
+
